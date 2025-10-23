23c8f524e010ff1491f6cddd7238f79ba5ba5afa
Fahad Siraj <fahadsiraj202281@gmail.com>
2025-10-24 04:43:27 +0530

    Minor Bugs Fix

--- src/main/java/org/example/luminex/BookController.java
+++ src/main/java/org/example/luminex/BookController.java
@@ -4,15 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 
 import java.util.Objects;
 
-public class CardController {
+public class BookController {
 
     @FXML
-    private HBox box;
+    private Label authorName;
 
     @FXML
     private ImageView bookImage;
@@ -20,24 +18,10 @@ public class CardController {
     @FXML
     private Label bookName;
 
-    @FXML
-    private Label authorName;
-
-
-    private final String[] colors = {"B9E5FF", "BDB2FE", "FB9AA8", "FF5056"};
     public void setData(Book book){
         Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(book.getImageSrc())));
-
         bookImage.setImage(image);
-
         bookName.setText(book.getName());
         authorName.setText(book.getAuthor());
-
-        box.setStyle(
-                "-fx-background-color: #" + colors[(int)(Math.random() * colors.length)] + ";" +
-                        "-fx-background-radius: 15;" +
-                        "-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.1), 10, 0, 0, 10);"
-        );
     }
-
 }
--- src/main/java/org/example/luminex/HelloController.java
+++ src/main/java/org/example/luminex/HelloController.java
@@ -3,7 +3,10 @@ package org.example.luminex;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,20 +18,42 @@ public class HelloController implements Initializable {
 
     @FXML
     private HBox cardLayout;
+
+
+    @FXML
+    private GridPane bookContainer;
+
     private List<Book> recentlyAdded;
+    private List<Book> recommended;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         recentlyAdded = new ArrayList<>(recentlyAdded());
+        recommended = new ArrayList<>(books());
+        int column=0, row=1;
         try{
-            for(int i=0; i<recentlyAdded.size(); i++){
+            for (Book value : recentlyAdded) {
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("card.fxml"));
                 HBox cardBox = fxmlLoader.load();
                 CardController cardController = fxmlLoader.getController();
-                cardController.setData(recentlyAdded.get(i));
+                cardController.setData(value);
                 cardLayout.getChildren().add(cardBox);
 
+            }
+            for (Book book : recommended){
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("book.fxml"));
+                VBox bookBox = fxmlLoader.load();
+                BookController bookController = fxmlLoader.getController();
+               bookController.setData(book);
+
+                if(column==6){
+                    column=0;
+                    ++row;
+                }
+                bookContainer.add(bookBox, column++, row);
+                GridPane.setMargin(bookBox, new Insets(10));
             }
         }catch (IOException e){
             e.printStackTrace();
@@ -36,6 +61,29 @@ public class HelloController implements Initializable {
     }
 
     private List<Book> recentlyAdded(){
+        List<Book> ls = new ArrayList<>();
+        Book book = new Book();
+        book.setName("Rich Dad\nPoor Dad");
+        book.setImageSrc("/org/example/luminex/img/no-cats-in-the-library-9781665933681_lg.jpg");
+        book.setAuthor("Robert T.Kiyosaki");
+        ls.add(book);
+
+
+        book = new Book();
+        book.setName("Atomic Habits");
+        book.setImageSrc("/org/example/luminex/img/no-cats-in-the-library-9781665933681_lg.jpg");
+        book.setAuthor("James Clear");
+        ls.add(book);
+
+        book = new Book();
+        book.setName("The Alchemist");
+        book.setImageSrc("/org/example/luminex/img/no-cats-in-the-library-9781665933681_lg.jpg");
+        book.setAuthor("Paulo Coelho");
+        ls.add(book);
+        return ls;
+    }
+
+    private List<Book> books(){
         List<Book> ls = new ArrayList<>();
         Book book = new Book();
         book.setName("Rich Dad\nPoor Dad");
